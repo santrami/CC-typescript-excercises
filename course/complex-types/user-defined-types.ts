6a0a4ea268406b3a4e4bb648592342fb4a22acad
santrami <santrami@hotmail.com>
2023-03-03 19:58:11 +0100

    complete structure course

---
+++ course/complex-types/user-defined-types.ts
@@ -0,0 +1,26 @@
+let petOnSale = 'chinchilla';
+let ordersArray = [
+  ['rat', 2], 
+  ['chinchilla', 1], 
+  ['hamster', 2], 
+  ['chinchilla', 50]
+];
+
+// Write your code below:
+enum Pet {
+  Hamster,
+  Rat,
+  Chinchilla,
+  Tarantula
+}
+
+let petOnSaleTS:Pet=Pet.Chinchilla;
+
+let ordersArrayTS:[Pet,number][] = [
+  [Pet.Rat,2],
+  [Pet.Chinchilla,1],
+  [Pet.Hamster,2],
+  [Pet.Chinchilla,50]
+]
+
+//ordersArrayTS.push([Pet.Jerboa, 3]);
